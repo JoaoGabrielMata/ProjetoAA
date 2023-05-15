a1454cf92a8d89aaaedfc1db5f57095f79d4f2c2
Estagiario_fi <estagiario_fi@sense.com.br>
2023-05-15 09:49:40 -0300

    Adicionei um teste de um aplicativo em dart

---
+++ Aplicativo/AA.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Projeto AA',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'teste Projeto AA'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _message = '';
+
+  void _showMessage() {
+    setState(() {
+      _message = 'Olá, mundo!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pressione o botão abaixo para exibir uma mensagem:',
+            ),
+            Text(
+              _message,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showMessage,
+        tooltip: 'Exibir mensagem',
+        child: Icon(Icons.message),
+      ),
+    );
+  }
+}
