218a381ea0bb03a44c7bc6cf9275ccfe030008dc
Estagiario_fi <estagiario_fi@sense.com.br>
2023-06-01 09:57:52 -0300

    Alteração

--- Esp32/TesteTimer/TesteTimer.ino
+++ Esp32/TesteTimer/TesteTimer.ino
@@ -1,26 +1,31 @@
-//Sistema de contagem
-unsigned long tempoInicial = 0; 
-
 void setup() {
-  tempoInicial = millis();
-  pinMode(4,INPUT);
+  pinMode(4,OUTPUT);
   pinMode(22,OUTPUT);
   pinMode(23,INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  //Verificando se o sistema funciona ou não
-
-  Serial.println(millis());
-
-  //Fazendo o teste da peça
+  //pino 22 = relé
+  //pino 23 = sensor
+  //pino 4 = led
   
-  if(digitalRead(23)==1){
-    digitalWrite(22,0);
+//// Teste da peça com o relé ////
+  if(digitalRead(23)==1){ //Se o sensor identificar alguma coisa
+    digitalWrite(22,0); //Ele aciona o relé, que nesse caso é ativado com 0
   }
-  else{
-    digitalWrite(22,1);
+  else{ //Caso não identifique nada
+    digitalWrite(22,1); //Ele simplesmente deixa o relé parado
   }
+//////////////////////////////////
 
+//Teste 
+if(digitalRead(22)==0){
+  digitalWrite(4,1);
+  delay(3000);
+  digitalWrite(4,0);
+}
+else{
+  digitalWrite(4,0);
+}
 }
