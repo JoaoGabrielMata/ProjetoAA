01acd8273e14ea6a60f3ebda78a1249b597d8234
Estagiario_fi <estagiario_fi@sense.com.br>
2023-06-06 11:21:12 -0300

    Alteração no projeto

--- Esp32/TesteTimer/TesteTimer.ino
+++ Esp32/TesteTimer/TesteTimer.ino
@@ -1,56 +1,41 @@
-unsigned long startTime = 0; // Variável para armazenar o tempo de início da contagem
-unsigned long elapsedTime = 0; // Variável para armazenar o tempo decorrido
-unsigned long pieceStartTime = 0; // Variável para armazenar o tempo de início da peça
-const unsigned long GOOD_PIECE_TIME = 2000; // Tempo em milissegundos para considerar uma peça boa
-const unsigned long DETECTION_DELAY = 500;  // Atraso em milissegundos após a detecção da peça
+unsigned long startTime = 0;  // Variável para armazenar o tempo de início da contagem
+unsigned long elapsedTime = 0;  // Variável para armazenar o tempo decorrido
 
-bool pieceDetected = false; // Variável para indicar se uma peça foi detectada
+bool pieceDetected = false;  // Variável para indicar se uma peça foi detectada
 
 void setup() {
-  pinMode(4, OUTPUT); // LED
-  pinMode(22, OUTPUT); // RELÉ
-  pinMode(23, INPUT); // SENSOR
-  pinMode(5, INPUT_PULLUP); // BOTÃO
+  pinMode(4, OUTPUT);   // LED
+  pinMode(22, OUTPUT);  // RELÉ
+  pinMode(23, INPUT);   // SENSOR
+  pinMode(5, INPUT_PULLUP);  // BOTÃO
   Serial.begin(9600);
 
-  startTime = millis(); // Inicializa o tempo de início da contagem
+  startTime = millis() / 1000;  // Inicializa o tempo de início da contagem em segundos
 }
 
 void loop() {
-
   if (digitalRead(23) == 1) {  // Se o sensor identificar alguma coisa
-    digitalWrite(22, 0);  // Ele aciona o relé, que nesse caso é ativado com 0
+    digitalWrite(22, 0);  // Aciona o relé (baixo)
     if (!pieceDetected) {
-      pieceStartTime = millis(); // Armazena o tempo de início da peça, apenas na detecção inicial da peça
-      pieceDetected = true; // Indica que uma peça foi detectada
+      startTime = millis() / 1000;  // Armazena o tempo de início da contagem em segundos, apenas na detecção inicial da peça
     }
-  } else { // Caso não identifique nada
-    digitalWrite(22, 1); // Ele simplesmente deixa o relé parado
-    pieceDetected = false; // Indica que nenhuma peça foi detectada
+    pieceDetected = true;  // Indica que uma peça foi detectada
+  } else {  // Caso não identifique nada
+    digitalWrite(22, 1);  // Desliga o relé (alto)
+    pieceDetected = false;  // Indica que nenhuma peça foi detectada
   }
 
-  unsigned long currentTime = millis();  // Obtém o tempo atual
+  unsigned long currentTime = millis() / 1000;  // Obtém o tempo atual em segundos
   elapsedTime = currentTime - startTime;  // Calcula o tempo decorrido
 
   Serial.print("Tempo decorrido: ");
   Serial.print(elapsedTime);
-  Serial.println(" milissegundos");
+  Serial.println(" segundos");
 
   if (pieceDetected) {
-    digitalWrite(4, 1); // Liga o LED para que o operador identifique
-    delay(DETECTION_DELAY); // Atraso após a detecção da peça
-
-    if (digitalRead(22) == 0) { // Caso o relé acione
-      unsigned long pieceElapsedTime = currentTime - pieceStartTime; // Calcula o tempo decorrido da peça
-      if (pieceElapsedTime <= GOOD_PIECE_TIME) { // Verifica se o tempo decorrido da peça é maior ou igual ao tempo para peça boa
-        // Peça boa
-        Serial.println("Peça Boa");
-      } else {
-        // Peça ruim
-        Serial.println("Peça Ruim");
-      }
-      delay(3000);  // Espera 3s
-      pieceDetected = false;  // Reinicia a detecção de peça
+    digitalWrite(4, 1);  // Liga o LED para que o operador identifique
+    if (digitalRead(22) == 0) {  // Caso o relé acione
+      delay(3000);  // Espera 3 segundos
     }
     digitalWrite(4, 0);  // Apaga o LED
   } else {
