3a0395ddbbd9499f35cfd3e7a1fb847011e32a47
Estagiario_fi <estagiario_fi@sense.com.br>
2023-06-07 08:51:53 -0300

    Primeiros passos do Aplicxativo versão final

--- Aplicativo/AA.dart
+++ Aplicativo/AA.dart
@@ -1,67 +1,123 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Projeto AA',
+      title: 'Meu Aplicativo',
       theme: ThemeData(
-        primarySwatch: Colors.black,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'teste Projeto AA'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _message = '';
-
-  void _showMessage() {
-    setState(() {
-      _message = 'Funciona';
-    });
-  }
-
- 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Página Inicial'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Pressione o botão abaixo para exibir uma mensagem:',
+          children: [
+            ElevatedButton(
+              child: Text('Cadastro'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CadastroPage()),
+                );
+              },
             ),
-            Text(
-              _message,
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Conferir'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ConferirPage()),
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showMessage,
-        tooltip: 'Pressione',
-        child: Icon(Icons.message),
+    );
+  }
+}
+
+class CadastroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Op',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Digite uma data',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Salvar'),
+              onPressed: () {
+                //adicionar a lógica para salvar os dados
+              },
+            ),
+          ],
+        ),
       ),
     );
-    floatingActionButton: floatingActionButton(
-      onPressed: 
-    )
+  }
+}
+
+class ConferirPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Conferir'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Pesquisar informações',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Pesquisar'),
+              onPressed: () {
+                // adicionar a lógica para buscar as informações
+              },
+            ),
+            SizedBox(height: 20),
+            //  exibir as informações encontradas
+          ],
+        ),
+      ),
+    );
   }
 }
